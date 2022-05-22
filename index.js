faabb2638bb96d2a99baecfc3ff5a1cd6b6f1025
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-05-22 00:56:50 -0400

    commit tree 5b1cfcf7cf5c4cabceb5c1980acebf2a0b437cd0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const newArray = [];
+
+
+function writeCards(names, event) {
+    for (let a = 0; a < names.length; a++) {
+    newArray.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`);
+    // debugger;
+    }
+
+  return newArray;
+}
+
+writeCards ();
+
+function countDown(int) {
+    let a=10;
+    while (a >= 0) {
+        console.log(a);
+        a--
+    }
+    return int;
+}
